9852eaf9f00b1b564cde97f0c7d2f54f57f5e35d
DanielRivasMD <danielrivasmd@gmail.com>
2021-04-19 02:24:45 +0200

    add cli by clap

---
+++ src/utils/cli.rs
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// standard libraries
+use clap::{crate_authors, crate_version, App, AppSettings};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn cli_mnemosyne() -> App<'static> {
+  App::new("mnemosyne")
+    .version(crate_version!())
+    .author(crate_authors!())
+    .setting(AppSettings::ArgRequiredElseHelp)
+    .setting(AppSettings::ColoredHelp)
+    .about(
+      "memory godess
+
+    ",
+    )
+    //.subcommand(
+    //App::new("clio")
+    //.about("clio")
+    //.setting(AppSettings::ArgRequiredElseHelp)
+    //.setting(AppSettings::ColoredHelp)
+    //.subcommand(App::new("prueba").about("nested")),
+    //)
+    .subcommand(App::new("new"))
+    .version(crate_version!())
+    .author(crate_authors!())
+    .about("")
+  //.subcommand(App::new("edit"))
+  // .version(crate_version!())
+  // .author(crate_authors!())
+  // .about("")
+  // .subcommand(App::new("ls"))
+  // .version(crate_version!())
+  // .author(crate_authors!())
+  // .about("")
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
