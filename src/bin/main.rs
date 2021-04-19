9344e7c165626a1c24aa74e8ed6f798d8df930f0
DanielRivasMD <danielrivasmd@gmail.com>
2021-04-19 02:24:23 +0200

    update main

---
+++ src/bin/main.rs
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Mnenosyne wrapper
+use mnemosyne::*;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use anyhow::Result as anyResult;
+use rusqlite::NO_PARAMS;
+use rusqlite::{Connection, Result};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use crate::utils::cli::cli_mnemosyne;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+fn main() -> anyResult<()> {
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  let matches = cli_mnemosyne().get_matches();
+
+  // TODO: check database exits ? update : create
+  // TODO: use commands to manipulate database
+  // TODO: build interfase
+  // TODO: add tests
+  // TODO: error handlers
+
+  let path = String::from("/Users/drivas/.mnemosyne.db");
+  let conn = Connection::open(path)?;
+  up(&conn)?;
+
+  if let Some(matches) = matches.subcommand_matches("new") {
+    ctrls::new::mnemosyne_new(matches, &conn);
+    // ctrls::clio::mnemosyne_clio(matches)?;
+  }
+
+  //if let Some(matches) = matches.subcommand_matches("clio") {
+  //ctrls::clio::mnemosyne_clio(matches);
+  //// ctrls::clio::mnemosyne_clio(matches)?;
+  //}
+
+  Ok(())
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn up(conn: &Connection) -> Result<()> {
+  conn.execute(
+    "create table if not exists events (
+     id integer primary key autoincrement,
+    summary text not null,
+    done bool not null
+         )",
+    NO_PARAMS,
+  )?;
+  Ok(())
+}
